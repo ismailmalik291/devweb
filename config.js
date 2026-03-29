c0ee3266c917d0c60c1911402a5b14387d949d0d
ismailmalik291 <ismailmalik1091@gmail.com>
2026-03-29 19:56:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,9 +9,9 @@ const CONFIG = {
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjZ29hZ3ZueWFudGZzdnlnZWlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyMDA3NjAsImV4cCI6MjA4OTc3Njc2MH0.2WJNvrTsaMY4VuE2Rf6At1wUsFHbfpQnNLwSko0tLOo",
 
   // GEMINI AI
-  GEMINI_API_KEY: "AIzaSyB36E3iSxg2oo0O5h1xTzFiGsHHehN4qMU",
-  GEMINI_MODEL: "gemini-2.0-flash",
-
+ GROQ_API_KEY: "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",
+ GROQ_MODEL: "llama-3.3-70b-versatile",
+ GROQ_URL: "https://api.groq.com/openai/v1/chat/completions",
   // STRIPE
   STRIPE_PUBLISHABLE_KEY: "pk_test_51TDvr97MB7f11AdGltf3VcJXSbh4gBzd9LeWRZf0A0U8HxKBU28z7sNMiwgEX7Fscud6JL37iiEAi1rKOdPZWNpr00GzQsFt6E",
 
